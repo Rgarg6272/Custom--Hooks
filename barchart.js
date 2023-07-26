b3ec47c8d56e2d7dd7ba3678412919196b59a5f9
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-07-26 22:37:55 +0530

    Create barchart.js

---
+++ barchart.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+const BarChart2 = () => {
+  return (
+    <div>
+      <Bar
+        data={{
+          labels: [
+            "24th",
+            "25th",
+            "26th",
+            "27th",
+            "28th",
+            "29th",
+            "30th",
+            "1st",
+            "2nd",
+            "3rd",
+          ],
+          datasets: [
+            {
+              label: "In Progress",
+              data: [75, 50, 75, 25, 125, 25, 75, 25, 125, 75],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+        height={200}
+        width={400}
+        options={{
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default BarChart2;
