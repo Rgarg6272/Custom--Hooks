62921ceb8b47e35ba585fb38a7b6446a62854eef
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-09-20 11:43:18 +0530

    Create tableData.jsx

---
+++ tableData.jsx
@@ -0,0 +1,22 @@
+export const DelegatedLevelTabletData = [
+  {
+    id: 0,
+    Entity: "PMS",
+    CC_Level: 3,
+    CC_Level_Start: '01-01-2023',
+    CC_Level_End: '01-05-2023',
+    Reason_For_End: "L3-NFLOC-Start",
+    Action: "",
+    enableDatePicker: false
+  },
+  {
+    id: 1,
+    Entity: "CC",
+    CC_Level: 7,
+    CC_Level_Start: '01-01-2023',
+    CC_Level_End: '01-01-2023',
+    Reason_For_End: "L7-CLPMSF-CCL_End",
+    Action: "",
+    enableDatePicker: false
+  },
+]
