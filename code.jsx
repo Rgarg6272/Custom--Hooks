e1897cbe5463e874f3d307e78d28f5de39ad692a
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-09-22 16:12:14 +0530

    Update code.jsx

--- code.jsx
+++ code.jsx
@@ -1,42 +1,41 @@
-import React, { useState } from "react";
-import DynamicTable from "./component/DynamicTable";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-const App = () => {
-  const [columns, setColumns] = useState([
-    {
-      title: "ID",
-      field: "id",
-      align: "center",
-      cellStyle: {
-        textDecoration: "underline",
-        cursor: "pointer",
-      },
-    },
-    { title: "Name", field: "name", align: "center" },
-    { title: "Age", field: "age", type: "numeric", align: "center" },
-    { title: "Gender", field: "gender", type: "string", align: "center" },
-    { title: "Phone", field: "phone", type: "numeric", align: "center" },
-  ]);
-
-  const [data, setData] = useState([
-    { id: 8358083, name: "John", age: 30, gender: "Male", phone: 9876789901 },
-    { id: 8358084, name: "Jone", age: 20, gender: "Male", phone: 9876789901 },
-    { id: 8358085, name: "Krish", age: 23, gender: "Male", phone: 9876789901 },
-    { id: 8358086, name: "Max", age: 43, gender: "Male", phone: 9876789901 },
-  ]);
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={<DynamicTable columns={columns} data={data} />}
-          />
-        </Routes>
-      </Router>
-    </>
-  );
-};
-
-export default App;
+ const formattedData = (item, MemberInfoData) => {
+        if (MemberInfoData.Name_and_dob) {
+            if (item.id === 'First_name') {
+                return MemberInfoData.Name_and_dob['First_name'];
+            } else if (item.id === 'Middle_name') {
+                return MemberInfoData.Name_and_dob['Middle_name'];
+            } else if (item.id === 'Last_name') {
+                return MemberInfoData.Name_and_dob['Last_name'];
+            } else if (item.id === 'Dob') {
+                return MemberInfoData.Name_and_dob['Dob'];
+            }
+        } else if (MemberInfoData.Identifiers) {
+            if (item.id === 'Subscriber_id') {
+                return MemberInfoData.Name_and_dob['First_name'];
+            } else if (item.id === 'Jiva_id') {
+                return MemberInfoData.Name_and_dob['Middle_name'];
+            } else if (item.id === 'Last_name') {
+                return MemberInfoData.Name_and_dob['Last_name'];
+            } 
+        } else if (MemberInfoData.Address) {
+            if (item.id === 'First_name') {
+                return MemberInfoData.Name_and_dob['First_name'];
+            } else if (item.id === 'Middle_name') {
+                return MemberInfoData.Name_and_dob['Middle_name'];
+            } else if (item.id === 'Last_name') {
+                return MemberInfoData.Name_and_dob['Last_name'];
+            } else if (item.id === 'Dob') {
+                return MemberInfoData.Name_and_dob['Dob'];
+            }
+        } else if (MemberInfoData.Contact_details) {
+            if (item.id === 'First_name') {
+                return MemberInfoData.Name_and_dob['First_name'];
+            } else if (item.id === 'Middle_name') {
+                return MemberInfoData.Name_and_dob['Middle_name'];
+            } else if (item.id === 'Last_name') {
+                return MemberInfoData.Name_and_dob['Last_name'];
+            } else if (item.id === 'Dob') {
+                return MemberInfoData.Name_and_dob['Dob'];
+            }
+        }
+    }
