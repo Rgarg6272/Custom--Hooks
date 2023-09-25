0c19e9d53a65f5fcc369096c8085f25b6a8bcdcb
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-09-25 12:37:29 +0530

    Update code.jsx

--- code.jsx
+++ code.jsx
@@ -1,52 +1,97 @@
-export const MemberInfoDetails = [
-  {
-    id: "First_name",
-    label: "First Name",
-    value: "",
-  },
-  {
-    id: "Middle_name",
-    label: "Middle Name",
-    value: "",
-  },
-  {
-    id: "Last_name",
-    label: "Last Name",
-    value: "",
-  },
-  {
-    id: "Dob",
-    label: "Date of Birth",
-    value: "",
+const formattedData = (id, data) => {
+  switch (id) {
+    case "First_name":
+      return data.Name_and_dob?.First_name || "-";
+
+    case "Middle_name":
+      return data.Name_and_dob?.Middle_name || "-";
+
+    case "Last_name":
+      return data.Name_and_dob?.Last_name || "-";
+
+    case "Dob":
+      return data.Name_and_dob?.Dob || "-";
+
+    case "Subscriber_id":
+      return data.Identifiers?.Subscriber_id || "-";
+
+    case "Medicaid_id":
+      return data.Identifiers?.Medicaid_id || "-";
+
+    case "Jiva_id":
+      return data.Identifiers?.Jiva_id || "-";
+
+    case "Address":
+      return data.Address || "-";
+
+    case "Contact_phone_number":
+      const phoneNumber = data.Contact_details?.Contact_phone_number || "-";
+
+      return formatPhoneNumber(phoneNumber);
+
+    default:
+      return "-";
   }
-];
-export const MemberIdentifier = [
-  {
-    id: "Subscriber_id",
-    label: "Subscriber ID",
-    value: "",
-  },
-  {
-    id: "Medicaid_id",
-    label: "Medicaid ID",
-    value: "",
-  },
-  {
-    id: "Jiva_id",
-    label: "Jiva ID",
-    value: "",
+};
+
+// Helper function to format phone numbers
+
+const formatPhoneNumber = (phoneNumber) => {
+  const cleaned = ("" + phoneNumber).replace(/\D/g, "");
+
+  const match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+
+  if (match) {
+    const intlCode = match[1] ? "+1 " : "";
+
+    return [intlCode, "(", match[2], ") ", match[3], "-", match[4]].join("");
   }
-];
-export const MemberAddress = [
-  {
-    id: "Address",
-    value: '',
-  },
-];
-export const MemberContact = [
-  {
-    id: "Contact_phone_number",
-    label: "Home",
-    value: '',//(505) 387-9088
-  },
-];
+
+  return phoneNumber;
+};
+
+// Inside the useEffect
+
+useEffect(() => {
+  console.log("MemberInfoData::", MemberInfoData);
+
+  // Set memberData
+
+  setMemberData(
+    MemberInfoDetails.map((item) => ({
+      ...item,
+
+      value: formattedData(item.id, MemberInfoData),
+    }))
+  );
+
+  // Set memberIdentify
+
+  setMemberIdentify(
+    MemberIdentifier.map((item) => ({
+      ...item,
+
+      value: formattedData(item.id, MemberInfoData),
+    }))
+  );
+
+  // Set memberAddress
+
+  setMemberAddress(
+    MemberAddress.map((item) => ({
+      ...item,
+
+      value: formattedData(item.id, MemberInfoData),
+    }))
+  );
+
+  // Set memberContact
+
+  setMemberContact(
+    MemberContact.map((item) => ({
+      ...item,
+
+      value: formattedData(item.id, MemberInfoData),
+    }))
+  );
+}, []);
